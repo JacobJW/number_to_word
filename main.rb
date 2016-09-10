ce3c93f93309e967b53b2eba5da33751d8df8577
Jacob Wernerowski <jacob.wernerowski@mail.utoronto.ca>
2016-09-09 19:22:11 -0700

    turns out I can't spell "forty"

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ $number_to_word= {
   6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten",
   11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen",
   15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen",
-  19 => "nineteen", 20 => "twenty", 30 => "thirty", 40 => "fourty",
+  19 => "nineteen", 20 => "twenty", 30 => "thirty", 40 => "forty",
   50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty",
   90 => "ninety", 100 => "one hundred", 200 => "two hundred",
   300 => "three hundred", 400 => "four hundred", 500 => "five hundred",
