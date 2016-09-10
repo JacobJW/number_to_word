c7be9929f62a19375ea2e705f79eac9df3d154aa
Jacob Wernerowski <jacob.wernerowski@mail.utoronto.ca>
2016-09-09 19:20:54 -0700

    fixed special case for 100's
    Fixed a case where a number like 215 was outputted as "two hundred and one five" instead of "two hundred and fifteen"

--- main.rb
+++ main.rb
@@ -36,15 +36,20 @@ def number_to_words(number)
     # Using remainder here allows us to reuse parts of
     # the code from 20 <= n < 100 case
     remainder = number % 100
-    # Checks how many 10's there are, if remainder is > 9
-    second = $number_to_word.fetch(((remainder / 10) * 10)) if remainder > 9
+    # Handles special case where remainder is a -teens number.
+    if (remainder >= 10) && (remainder < 20)
+      second = $number_to_word.fetch(remainder)
+      return "#{first} and #{second}"
+    end
+    # Checks how many 10's there are, if remainder is >= 20
+    second = $number_to_word.fetch(((remainder / 10) * 10)) if remainder >= 20
     # Checks how many one's there are, if any.
     third = $number_to_word.fetch(remainder % 10) if remainder % 10 != 0
     # three cases: x00, xx0, xxx
     if remainder == 0
       return first
     elsif remainder % 10 == 0
-      return "#{first}and #{second}"
+      return "#{first} and #{second}"
     else
       return "#{first} and #{second} #{third}"
     end
